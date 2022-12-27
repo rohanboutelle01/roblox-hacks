f98c0f6ec54523c8942917d256dcf66a68cba80e
Rohan Boutelle <105883535+RohanBoutelle@users.noreply.github.com>
2022-12-27 15:11:46 -0500

    Create teleport-gui.lua

---
+++ teleport-gui.lua
@@ -0,0 +1,2 @@
+-- teleport gui script
+loadstring(game:HttpGet("https://gist.githubusercontent.com/DagerFild/b4776075a0d26ef04394133ee6bd2081/raw/0ed51ac94057d2d9a9f00e1b037b9011c76ca54a/tpGUI", true))()
