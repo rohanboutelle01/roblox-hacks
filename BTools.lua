db1a6cb6368f97768d77c4067333660eba8d919c
Rohan Boutelle <105883535+RohanBoutelle@users.noreply.github.com>
2022-12-27 15:16:56 -0500

    BTools
    NOTE: 
    BTools will only effect your screen. Meaning that it will not effect the other players.

---
+++ BTools.lua
@@ -0,0 +1,18 @@
+-- B Tools script
+
+backpack = game:GetService("Players").LocalPlayer.Backpack
+
+hammer = Instance.new("HopperBin")
+hammer.Name = "Hammer"
+hammer.BinType = 4
+hammer.Parent = backpack
+
+cloneTool = Instance.new("HopperBin")
+cloneTool.Name = "Clone"
+cloneTool.BinType = 3
+cloneTool.Parent = backpack
+
+grabTool = Instance.new("HopperBin")
+grabTool.Name = "Grab"
+grabTool.BinType = 2
+grabTool.Parent = backpack
