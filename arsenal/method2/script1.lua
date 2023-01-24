3f442f4a4b035b81ba78fecf5a289e8906024016
Rohan Boutelle <105883535+rohanboutelle01@users.noreply.github.com>
2023-01-24 14:41:39 -0500

    Update script1.lua

--- arsenal/method2/script1.lua
+++ arsenal/method2/script1.lua
@@ -1,7 +1,7 @@
-_G.lockMeth = "Random" -- Change to head if you want your silent aim to ONLY aim at the head.
+_G.lockMeth = "Head" -- Change to head if you want your silent aim to ONLY aim at the head.
 _G.silMeth = "FPORWIL" -- Silent Aim Method, You're gonna have to play around with it. The Default one is FindPartOnRayWithIgnoreList but shortened. 
 _G.AntiFly = false -- Change if the game you're playing has an Anti-Fly, set it to false, if it has, set it to true, if it doesnt.
-_G.Keystrokes = true -- If you want sexy keystrokes, turn it on.
+_G.Keystrokes = true -- If you want #### keystrokes, turn it on.
 _G.HideOverlay = false -- If you want the StormWare Watermark along with the features you've toggled shown, set it to true, not recommended for closet cheating, obviously.
 
 
@@ -12,5 +12,5 @@ if _G.HideOverlay == true then
     game.CoreGui["StormWare LITE"].modules.Visible = false
 end
 
--- I have to set my keystrokes manually
+-- I have to set my keystrokes manually 
 game.CoreGui["StormWare LITE"].keystrokes.space.Text = "âââââ"
